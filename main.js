55d7b1e73fe5dbbe4fc9bc436c28b4019542ee1a
KingMaker1307 <121443296+KingMaker1307@users.noreply.github.com>
2023-03-06 18:37:10 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+
 function setup(){
     canvas = createCanvas(300,300);
-    canvas.position(530,160)
+    canvas.position(530,160);
+    video = createCapture(VIDEO);
+    video.size(300,300)
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose",Poses);
 }
 function draw(){
+    image(video,0,0,300,300);
 }
-function preload(){
-    
+function Poses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x =",results[0].pose.nose.x);
+        console.log("nose y =",results[0].pose.nose.y);
+    }
 }
-function take_snapshot(){
-    
+function modelLoaded(){
+    console.log(modelLoaded);
 }
