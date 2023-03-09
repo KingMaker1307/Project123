1bea3ea6fe62d7bd26201d550ecbea8bc9bffa66
KingMaker1307 <121443296+KingMaker1307@users.noreply.github.com>
2023-03-09 06:11:48 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+noseX=0;
+noseY=0;
 function setup(){
     canvas = createCanvas(300,300);
     canvas.position(530,160);
@@ -8,14 +9,20 @@ function setup(){
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on("pose",Poses);
 }
+function preload(){
+    clown_nose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
 function draw(){
     image(video,0,0,300,300);
+    image(clown_nose,noseX-22,noseY+6,45,35);
 }
 function Poses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x =",results[0].pose.nose.x);
-        console.log("nose y =",results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x ="+noseX);
+        console.log("nose y ="+noseY);
     }
 }
 function modelLoaded(){
